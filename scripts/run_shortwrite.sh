9488026cf30c15b5a15295c2dae296c30c2f6cf6
ibiswas <ibiswas@aerospike.com>
2024-09-18 14:45:29 -0700

    remove mvn from run scripts

--- scripts/run_shortwrite.sh
+++ scripts/run_shortwrite.sh
@@ -1,4 +1,3 @@
 # This must be run from the root directory of the project
 DIR_PATH=$(pwd)
-mvn clean install
 java -Dconfig=$DIR_PATH/conf/shortwrite.properties -jar ./target/tinkerBench-1.0-SNAPSHOT-jar-with-dependencies.jar BenchmarkWrite
